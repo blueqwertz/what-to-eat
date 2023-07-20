d09368043bcdf6d36990e2df47d1fa7df0bbcddd
blueqwertz <timlimlei@icloud.com>
2023-07-20 10:32:57 +0200

    added time selection

--- src/components/mealsearch/meal-search.tsx
+++ src/components/mealsearch/meal-search.tsx
@@ -2,11 +2,23 @@ import { api } from "~/utils/api";
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTable } from "../ui/datatable";
 import { Button } from "../ui/button";
-import { History, Meal } from "@prisma/client";
+import { History, Meal, TimeOfDay } from "@prisma/client";
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { timeOfDayNameMap, unitMap } from "~/utils/enumMaps";
+import { Clock8 } from "lucide-react";
 
 function MealSearch() {
+  const [timeOfDay, setTimeOfDay] = useState<TimeOfDay>(TimeOfDay.NOON);
+
   const { data: mealToEat, isLoading } = api.meals.findMeal.useQuery({
-    data: { timeOfDay: "NOON" },
+    data: { timeOfDay },
   });
 
   if (isLoading) {
@@ -17,7 +29,33 @@ function MealSearch() {
     return <>Kein Gericht gefunden.</>;
   }
 
-  return <DataTable data={mealToEat} columns={columns} showPages={false} />;
+  return (
+    <>
+      <Select
+        value={timeOfDay}
+        onValueChange={(value: TimeOfDay) => {
+          setTimeOfDay(value);
+        }}
+      >
+        <SelectTrigger>
+          <div className="flex items-center gap-2">
+            <Clock8 className="h-4 w-4" />
+            <SelectValue />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          {Object.keys(timeOfDayNameMap).map((entry) => {
+            return (
+              <SelectItem value={entry} key={entry}>
+                {timeOfDayNameMap[entry]}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+      <DataTable data={mealToEat} columns={columns} showPages={false} />
+    </>
+  );
 }
 
 export default MealSearch;
